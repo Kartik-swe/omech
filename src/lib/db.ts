fa9495c41c7f46512f86b822f4e2b3f26b326b35
Kartik Shelke <kartikshelke284@gmail.com>
2024-12-03 09:11:08 +0530

    devdone

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,10 +3,10 @@ const sql = require('mssql');
 
 // Define the configuration for your database connection
 const config = {
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  server: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USER || '',
+  password: process.env.DATABASE_PASSWORD|| '',
+  server: process.env.DATABASE_HOST|| '',
+  database: process.env.DATABASE_NAME|| '',
   options: {
     encrypt: false, // Set to true if encryption is required
     trustServerCertificate: true, // Use true if connecting to a server with a self-signed certificate
